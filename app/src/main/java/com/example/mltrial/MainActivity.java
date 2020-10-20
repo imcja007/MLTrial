568a013302e441fc3a8fcf1ed2b5336e9b1d9668
Chaitanya J. Ajmera <cjajmera007@gmail.com>
2020-10-20 21:46:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mltrial/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.mltrial;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Button captureImage;
+    private ImageView imageView;
+    private TextView textView;
+    private InputImage image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        captureImage = findViewById(R.id.capture_image);
+        imageView = findViewById(R.id.image_view);
+        textView = findViewById(R.id.text_display);
+
+        captureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+
+            }
+        });
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageFromBitmap(imageBitmap);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void imageFromBitmap(Bitmap bitmap) {
+        int rotationDegree = 0;
+        // [START image_from_bitmap]
+        image = InputImage.fromBitmap(bitmap, rotationDegree);
+        // [END image_from_bitmap]
+
+        TextRecognizer recognizer = TextRecognition.getClient();
+        Task<Text> result =
+                recognizer.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text result) {
+                                // [START mlkit_process_text_block]
+                                String resultText = result.getText();
+                                System.out.println("RESULT TEXT=====================" + resultText);
+                                textView.setText(resultText);
+                                for (Text.TextBlock block : result.getTextBlocks()) {
+                                    String blockText = block.getText();
+                                    Point[] blockCornerPoints = block.getCornerPoints();
+                                    Rect blockFrame = block.getBoundingBox();
+                                    for (Text.Line line : block.getLines()) {
+                                        String lineText = line.getText();
+                                        System.out.println("Line TEXT=====================" + lineText);
+                                        Point[] lineCornerPoints = line.getCornerPoints();
+                                        Rect lineFrame = line.getBoundingBox();
+                                        for (Text.Element element : line.getElements()) {
+                                            String elementText = element.getText();
+                                            Point[] elementCornerPoints = element.getCornerPoints();
+                                            Rect elementFrame = element.getBoundingBox();
+                                            System.out.println("elemnt TEXT=====================" + elementText);
+                                        }
+                                    }
+                                }
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+    }
+
+
+}
